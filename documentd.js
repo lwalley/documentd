01e43e4563d45d521411b68cd730570f94adf28d
Kris Brandow <kris.brandow@gmail.com>
2013-12-10 23:17:33 -0500

    Adding ability to use an external file called documentd.json to pull in the markdown documents.

--- documentd.js
+++ documentd.js
@@ -16,6 +16,20 @@ $( document ).ready ( function() {
             title: 'RocketLauncher',
         }
     };
+    $.get('documentd.json', function(data) {
+        var documents = data.documents;
+        $.each(documents, function(key, value) {
+            menu.append('<li><a href="#" data-documentd="' + key + '" class="documentd-link">' + value.title + '</a></li>');
+        });
+        $(".documentd-link").click(function() {
+            var mkdFile = $(this).data("documentd");
+            var mkdPath = documentRoot + "/" + mkdFile;
+            $.get(mkdPath, function(data) {
+                html = marked(data);
+                $(".documentation").html(html);
+            });
+        });
+    });
     var menu = $('#documentd-navigation ul');
     $.each(documents, function(key, value) {
         menu.append('<li><a href="#" data-documentd="' + key + '" class="documentd-link">' + value.title + '</a></li>');
