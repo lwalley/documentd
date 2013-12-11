e85302673b76366ebd678a3251c3a7a3b480bd96
Kris Brandow <kris.brandow@gmail.com>
2013-12-10 23:19:44 -0500

    Updating javascript to use the documentd.json file to get the menu items and markdown settings.

--- documentd.js
+++ documentd.js
@@ -1,36 +1,23 @@
 console.log(marked('I am using __markdown__.'));
 $( document ).ready ( function() {
     $.ajaxSetup({ cache: false });
-    var documentRoot = 'infrastructure-doc';
-    var documents = {
-        'database_server.mkd': {
-            title: 'Database Server',
-        },
-        'gluster.mkd': {
-            title: 'GlusterFS',
-        },
-        'webserver.mkd': {
-            title: 'Web Server',
-        },
-        'rocketlauncher.mkd': {
-            title: 'RocketLauncher',
-        }
-    };
+    var documentRoot = 'infrastructure-doc',
+        menu = $('#documentd-navigation ul');
+
     $.get('documentd.json', function(data) {
         var documents = data.documents;
         $.each(documents, function(key, value) {
             menu.append('<li><a href="#" data-documentd="' + key + '" class="documentd-link">' + value.title + '</a></li>');
         });
         $(".documentd-link").click(function() {
-            var mkdFile = $(this).data("documentd");
-            var mkdPath = documentRoot + "/" + mkdFile;
+            var mkdFile = $(this).data("documentd"),
+                mkdPath = documentRoot + "/" + mkdFile;
             $.get(mkdPath, function(data) {
                 html = marked(data);
                 $(".documentation").html(html);
             });
         });
     });
-    var menu = $('#documentd-navigation ul');
     $.each(documents, function(key, value) {
         menu.append('<li><a href="#" data-documentd="' + key + '" class="documentd-link">' + value.title + '</a></li>');
     });
